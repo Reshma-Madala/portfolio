95d0f9914739abe256b5db7e411f0263d09b6bae
Reshma-Madala <m.vijayareshma@gmail.com>
2025-01-19 09:43:28 +0530

    Skills Section Functionality Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -29,6 +29,22 @@ function linkAction(){
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills__content');
+const skillsHeader = document.querySelectorAll('.skills__header');
+
+function toggleSkills() {
+    let itemClass = this.parentNode.className;
+    for (let i = 0; i < skillsContent.length; i++) {
+        skillsContent[i].className = 'skills__content skills__close';
+    }
+    if (itemClass === 'skills__content skills__close') {
+        this.parentNode.className = 'skills__content skills__open';
+    }
+}
+
+skillsHeader.forEach((elem) => {
+    elem.addEventListener('click', toggleSkills);
+});
 
 
 /*==================== QUALIFICATION TABS ====================*/
