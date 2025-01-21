a314286f027f738fc2d5ba6b06579e981f1bd50c
Reshma-Madala <m.vijayareshma@gmail.com>
2025-01-21 14:02:34 +0530

    Qualification Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -46,9 +46,26 @@ skillsHeader.forEach((elem) => {
     elem.addEventListener('click', toggleSkills);
 });
 
-
 /*==================== QUALIFICATION TABS ====================*/
+const tabs = document.querySelectorAll('[data-target]'),
+      tabContents = document.querySelectorAll('[data-content]')
 
+tabs.forEach(tab =>{
+    tab.addEventListener('click', ()=>{
+        const target = document.querySelector(tab.dataset.target)
+
+        tabContents.forEach(tabContent =>{
+            tabContent.classList.remove('qualification__active')
+
+        })
+        target.classList.add('qualification__active')
+
+        tabs.forEach(tab =>{
+            tab.classList.remove('qualification__active')
+        })
+        tab.classList.add('qualification__active')
+    })
+})
 
 /*==================== SERVICES MODAL ====================*/
 
