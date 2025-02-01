706e4e5f167826c041af682328f2e185f5566945
Reshma-Madala <m.vijayareshma@gmail.com>
2025-02-01 15:45:07 +0530

    Contact And Footer Added

--- assets/js/main.js
+++ assets/js/main.js
@@ -67,14 +67,38 @@ tabs.forEach(tab =>{
     })
 })
 
-/*==================== PROJECTS SWIPER  ====================*/
-
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+
+        const navLink = document.querySelector(".nav__menu a[href*=" + sectionId + "]");
+
+        if (navLink) { 
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                navLink.classList.add("active-link");
+            } else {
+                navLink.classList.remove("active-link");
+            }
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
 
 
 /*==================== CHANGE BACKGROUND HEADER ====================*/ 
-
+function scrollHeader(){
+    const nav = document.getElementById('header')
+    if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
 
 /*==================== SHOW SCROLL UP ====================*/ 
 
